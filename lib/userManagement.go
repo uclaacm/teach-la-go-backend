ce67142a288e4042fb314bc5812259ca8ccaf625
krashanoff <lkrashanoff2@gmail.com>
2019-11-13 23:00:27 -0800

    Use request context. Improve error management.

--- lib/userManagement.go
+++ lib/userManagement.go
@@ -1,12 +1,13 @@
 package lib
 
 import (
-	"context"
 	"encoding/json"
 	"log"
 	"net/http"
 
 	"cloud.google.com/go/firestore"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // UserData
@@ -28,6 +29,12 @@ func (h HandleUsers) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// parse the /:uid field
 	uid := r.URL.Path[len("/userData/"):]
 
+	// catch bad requests
+	if uid == "" && r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	// handle based on request method
 	switch r.Method {
 	case http.MethodGet:
@@ -46,20 +53,17 @@ func (h HandleUsers) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // Used to retrieve a user data object as JSON.
 func (h *HandleUsers) getUserData(w http.ResponseWriter, r *http.Request, uid string) {
 	// acquire userdoc corresponding to uid
-	userDoc, err := h.Client.Collection("users").Doc(uid).Get(context.Background())
+	userDoc, err := h.Client.Collection("users").Doc(uid).Get(r.Context())
 
-	if err != nil {
+	// catch document errors.
+	if status.Code(err) == codes.NotFound {
+		http.Error(w, "document does not exist.", http.StatusNotFound)
+		return
+	} else if err != nil {
 		log.Printf("error occurred in document retrieval: %s", err)
 		http.Error(w, "error occurred in document retrieval.", http.StatusInternalServerError)
 		return
 	}
-
-	if !userDoc.Exists() {
-		http.Error(w, "document does not exist.", http.StatusNotFound)
-		return
-	}
-
-	log.Printf("acquired user doc %s.", uid)
 
 	// acquire only desired fields for response.
 	var u UserData
@@ -71,6 +75,8 @@ func (h *HandleUsers) getUserData(w http.ResponseWriter, r *http.Request, uid st
 
 	// convert to JSON.
 	var userJSON []byte
+
+	// optional: pretty print our JSON response.
 	userJSON, err = json.MarshalIndent(u, "", "    ")
 	if err != nil {
 		log.Printf("error occurred in writing document to %T object.", u)
