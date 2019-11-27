d00c3da9727792985c0948176bae9576e2d03945
krashanoff <lkrashanoff2@gmail.com>
2019-11-27 09:58:03 -0800

    Improve readability for dbTools.

--- dbTools/dbTools.go
+++ dbTools/dbTools.go
@@ -10,15 +10,14 @@ import (
 	"google.golang.org/api/option"
 )
 
-/* GetDB
-Returns a pointer to a firestore Client based on the
-JSON credentials pointed to by the environment variable
-$CFGPATH.
-*/
-func GetDB() *firestore.Client {
-	// get client config. Fails early
-	// if we cannot find our config.
+// GetDB returns a pointer to a firestore *Client based on the
+// JSON credentials pointed to by the environment variable
+// $CFGPATH.
+func GetDB() (client *firestore.Client) {
+	// load environment variable $CFGPATH as a string: the path to our config.
 	configPath := os.Getenv("CFGPATH")
+
+	// check, using os.Stat(), that the file exists. If it does not exist, fail.
 	if _, err := os.Stat(configPath); err != nil {
 		log.Fatalf("could not find firebase config file! Did you set your CFGPATH variable? %s", err)
 	}
@@ -32,12 +31,12 @@ func GetDB() *firestore.Client {
 		log.Fatalf("failed to create client: %s", err)
 	}
 
-	// acquire the firestore client.
-	var client *firestore.Client
+	// acquire the firestore client, fail if we cannot.
 	client, err = app.Firestore(context.Background())
 	if err != nil {
 		log.Fatalf("failed to create client: %s", err)
 	}
 
-	return client
+	// naked return of "client" if successful.
+	return
 }
