2a393a00026bd185eb190fbdec93ff6b1cedecbc
krashanoff <lkrashanoff2@gmail.com>
2020-02-08 15:07:31 -0800

    Add helper functions to User for program and class management.

--- lib/user.go
+++ lib/user.go
@@ -1,6 +1,10 @@
 package lib
 
-import "cloud.google.com/go/firestore"
+import (
+	"fmt"
+
+	"cloud.google.com/go/firestore"
+)
 
 // User is a struct representation of a user document.
 // It provides functions for converting the struct
@@ -22,4 +26,70 @@ func (u *User) ToFirestoreUpdate() []firestore.Update {
 		{Path: "programs", Value: u.Programs},
 	}
 	return f
+}
+
+// AddClass adds a class id 'cid' to an User's class list.
+func (u *User) AddClass(cid string) {
+	u.Classes = append(u.Classes, cid)
+}
+
+// RemoveClass removes a class id 'cid' from an User's
+// class list. An error is thrown if the User is not
+// a member of that class.
+func (u *User) RemoveClass(cid string) error {
+	// loop through all the elements.
+	// when we find the index of a match, change
+	// the array to exclude the element, and then
+	// return nil.
+	for idx, el := range u.Classes {
+		// found the element.
+		if el == cid {
+			// remove it and update.
+			half := u.Classes[:idx]
+
+			// append elements of h2.
+			for _, element := range u.Classes[idx:] {
+				half = append(half, element)
+			}
+
+			u.Classes = half
+			return nil
+		}
+	}
+
+	// if we can't find it, throw an error.
+	return fmt.Errorf("failed to find %s in User classes", cid)
+}
+
+// AddProgram adds the program id 'pid' to an User's
+// program list.
+func (u *User) AddProgram(pid string) {
+	u.Programs = append(u.Programs, pid)
+}
+
+// RemoveProgram removes the program identified from the User's
+// array.
+func (u *User) RemoveProgram(pid string) error {
+	// loop through all the elements.
+	// when we find the index of a match, change
+	// the array to exclude the element, and then
+	// return nil.
+	for idx, el := range u.Programs {
+		// found the element.
+		if el == pid {
+			// remove it and update.
+			half := u.Programs[:idx]
+
+			// append elements of h2.
+			for _, element := range u.Programs[idx:] {
+				half = append(half, element)
+			}
+
+			u.Programs = half
+			return nil
+		}
+	}
+
+	// if we can't find it, throw an error.
+	return fmt.Errorf("failed to find %s in User programs", pid)
 }
