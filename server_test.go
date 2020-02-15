24f6d4f36e01ad1721b0205375e74308363f4cd9
tfukaza <tomokifukazawa123@gmail.com>
2020-02-14 19:56:15 -0800

    Improved formatting of test program. Moved it to the root directory and associated it with server.go

--- server_test.go
+++ server_test.go
@@ -1,41 +1,38 @@
-package tester
+package main_test
 
 import(
 	"context"
 	"bytes"
-	//"os"
 	"testing"
 	"net/http"
 	"net/http/httptest"
 	"encoding/json"
 	"io/ioutil"
-	"fmt"
 
-	"../lib"
+	"./lib"
 )
 
 // PORT defines where we serve the backend.
 const PORT = ":8081"
 
+//structure to store 
 type Response struct{
 	UserData lib.User 
 	Programs []string
 }
+
 //Runs series of test to test functionality of database
 func TestRigDB(t *testing.T) {
 
-	// set up context for main routine.
-	//mainContext := context.Background()
-
-	// acquire DB client.
-	// fails early if we cannot acquire one.
+	
 	var (
-		d   *lib.DB
-		err error
+		d   *lib.DB		// stores instance of connection with database 
+		err error		
+		res Response	// structure to store response fron database
 	)
 
-	var res Response
-
+	t.Logf("Testing initialization of database...")
+	
 	// Test opening connection with database
 	t.Run("Open connection with database", func(t *testing.T){
 
@@ -43,9 +40,8 @@ func TestRigDB(t *testing.T) {
 			t.Fatal("failed to open DB client")
 		}	
 	})
-	//defer d.Close()
-
 	
+	t.Logf("Testing creating new user...")
 
 	// Test creating a new user
 	t.Run("Create new user", func(t *testing.T){
@@ -62,35 +58,40 @@ func TestRigDB(t *testing.T) {
 		handler.ServeHTTP(rr, req)
 
 		if status := rr.Code; status != http.StatusOK {
-			t.Fatal("Failed to create users")
+			t.Fatal("Failed to create user")
 		}
 
+		// if creating user succeeded, record the response to use it in the next test 
 		defer rr.Result().Body.Close()
+
+		t.Logf("Create user successful")
+		t.Logf("Reading response")
 
 		j, err := ioutil.ReadAll(rr.Result().Body)
 		if err != nil {
-		 	t.Fatal("Failed to create users")
+		 	t.Fatal("Failed to read response")
 		}
 
 		json.Unmarshal([]byte(j), &res)
 
 	})
 
+	t.Logf("Testing deletion of program from user...")
 	
-
 	// Test deleting a program from a user
 	t.Run("Delete program", func(t *testing.T){
 
 		req, err := http.NewRequest("DELETE", "/program/delete", nil)
 
 		if err != nil {
-			t.Fatal("Failed to test create user")
+			t.Fatal("Failed to create http request")
 		}
 
+		// build query
 		p := req.URL.Query()
 
-		fmt.Printf("UID: %s\n", res.UserData.UID)
-		fmt.Printf("Program: %s\n", res.UserData.Programs[0])
+		//fmt.Printf("UID: %s\n", res.UserData.UID)
+		//fmt.Printf("Program: %s\n", res.UserData.Programs[0])
 		
 		p.Add("userId", res.UserData.UID)
 		p.Add("programId", res.UserData.Programs[0])
@@ -107,9 +108,12 @@ func TestRigDB(t *testing.T) {
 
 	})
 
+	t.Logf("Testing creating program for user...")
+
 	// Test creating a program from a user
 	t.Run("Create program", func(t *testing.T){
 
+		// create JSON for a new program 
 		pr := struct {
 			Code 		string 
 			DateCreated string
@@ -132,7 +136,7 @@ func TestRigDB(t *testing.T) {
 			t.Fatal("Failed to create JSON")
 		}
 
-		fmt.Printf("%s", pro)
+		//fmt.Printf("%s", pro)
 
 		req, err := http.NewRequest("POST", "/program/create", bytes.NewBuffer(pro))
 		req.Header.Set("Content-Type", "application/json")
