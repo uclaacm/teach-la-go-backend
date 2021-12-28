dc6bcff0a3125ae9e5475d0f62a5cc656ecb519f
Samarth Upadhyaya <chessisamazing27@gmail.com>
2021-12-28 22:47:39 +0800

    Commented out unused code from class_test.go

--- db/class_test.go
+++ db/class_test.go
@@ -1,24 +1,13 @@
 package db
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
-
-	"testing"
-
 	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-const (
-	colorInfo = "\033[32m"
-	colorEnd  = "\033[0m"
-)
+// const (
+// 	colorInfo = "\033[32m"
+// 	colorEnd  = "\033[0m"
+// )
 
 type TestFunc func(echo.Context) error
 
@@ -29,39 +18,39 @@ type TestObj struct {
 	User     []User
 }
 
-type ReqParam struct {
-	HTTPMethod string
-	Path       string
-	Body       io.Reader
-	Function   TestFunc // function to close response body
-	ExpCode    int      // expected return code.
-	Returns    bool     // specify if this call returns a body or not
-}
+// type ReqParam struct {
+// 	HTTPMethod string
+// 	Path       string
+// 	Body       io.Reader
+// 	Function   TestFunc // function to close response body
+// 	ExpCode    int      // expected return code.
+// 	Returns    bool     // specify if this call returns a body or not
+// }
 
-func CallFunc(t *testing.T, par *ReqParam) ([]byte, func() error) {
-	req := httptest.NewRequest(par.HTTPMethod,
-		par.Path,
-		par.Body)
-	rec := httptest.NewRecorder()
-	assert.NotNil(t, req, rec)
-	req.Header.Set("Content-Type", "application/json")
-	c := echo.New().NewContext(req, rec)
+// func CallFunc(t *testing.T, par *ReqParam) ([]byte, func() error) {
+// 	req := httptest.NewRequest(par.HTTPMethod,
+// 		par.Path,
+// 		par.Body)
+// 	rec := httptest.NewRecorder()
+// 	assert.NotNil(t, req, rec)
+// 	req.Header.Set("Content-Type", "application/json")
+// 	c := echo.New().NewContext(req, rec)
 
-	require.NoError(t, par.Function(c))
-	assert.Equal(t, par.ExpCode, rec.Code)
-	var b []byte
-	if par.Returns == true {
-		assert.NotEmpty(t, rec.Result().Body)
-		var err error
-		b, err = ioutil.ReadAll(rec.Result().Body)
-		require.NoError(t, err)
-		assert.NoError(t, err)
-	} else {
-		b = []byte{'{', '}'}
-	}
+// 	require.NoError(t, par.Function(c))
+// 	assert.Equal(t, par.ExpCode, rec.Code)
+// 	var b []byte
+// 	if par.Returns == true {
+// 		assert.NotEmpty(t, rec.Result().Body)
+// 		var err error
+// 		b, err = ioutil.ReadAll(rec.Result().Body)
+// 		require.NoError(t, err)
+// 		assert.NoError(t, err)
+// 	} else {
+// 		b = []byte{'{', '}'}
+// 	}
 
-	return b, rec.Result().Body.Close
-}
+// 	return b, rec.Result().Body.Close
+// }
 
 /* func CreateTestUser(t *testing.T, o *TestObj, i int) {
 
@@ -103,33 +92,33 @@ func CallFunc(t *testing.T, par *ReqParam) ([]byte, func() error) {
 // 	t.Logf(colorInfo+"Removed user %s"+colorEnd, o.User[i].UID)
 // }
 
-func CreateTestClass(t *testing.T, o *TestObj, classIndex int, userIndex int) {
-	pr := struct {
-		UID       string
-		Name      string
-		Thumbnail int
-	}{
-		o.User[userIndex].UID,
-		"TestClass",
-		1,
-	}
-	pro, err := json.Marshal(&pr)
-	require.NoError(t, err)
+// func CreateTestClass(t *testing.T, o *TestObj, classIndex int, userIndex int) {
+// 	pr := struct {
+// 		UID       string
+// 		Name      string
+// 		Thumbnail int
+// 	}{
+// 		o.User[userIndex].UID,
+// 		"TestClass",
+// 		1,
+// 	}
+// 	pro, err := json.Marshal(&pr)
+// 	require.NoError(t, err)
 
-	par := ReqParam{
-		"POST",
-		"/",
-		bytes.NewBuffer(pro),
-		o.D.CreateClass,
-		http.StatusOK,
-		true,
-	}
-	b, close := CallFunc(t, &par)
-	defer assert.NoError(t, close())
-	assert.NoError(t, json.Unmarshal([]byte(b), &o.Class[classIndex]))
+// 	par := ReqParam{
+// 		"POST",
+// 		"/",
+// 		bytes.NewBuffer(pro),
+// 		o.D.CreateClass,
+// 		http.StatusOK,
+// 		true,
+// 	}
+// 	b, close := CallFunc(t, &par)
+// 	defer assert.NoError(t, close())
+// 	assert.NoError(t, json.Unmarshal([]byte(b), &o.Class[classIndex]))
 
-	t.Logf(colorInfo+"CreateClass returned: \n%s"+colorEnd, string([]byte(b)))
-}
+// 	t.Logf(colorInfo+"CreateClass returned: \n%s"+colorEnd, string([]byte(b)))
+// }
 
 // func DeleteTestClass(t *testing.T, o *TestObj, classIndex int) {
 // 	pr := struct {
