60ddd61bddb4b99c6beced29472f95aafe95f5e7
Samarth Upadhyaya <chessisamazing27@gmail.com>
2022-01-07 15:38:52 -0800

    Wrote tests for JoinClass to cover the case where the class is already associated with the user

--- handler/class_test.go
+++ handler/class_test.go
@@ -504,6 +504,35 @@ func TestJoinClass(t *testing.T) {
 		assert.Equal(t, user.Classes[0], "test")
 	})
 	t.Run("classAlreadyWithUser", func(t *testing.T) {
+		d := db.OpenMock()
+		require.NoError(t, d.StoreClass(context.Background(), db.Class{
+			CID: "test",
+		}))
+		require.NoError(t, d.StoreUser(context.Background(), db.User{
+			UID:     "test",
+			Classes: []string{"test"},
+		}))
+		req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(`{"uid": "test", "cid": "test"}`))
+		rec := httptest.NewRecorder()
+		assert.NotNil(t, req, rec)
+		c := echo.New().NewContext(req, rec)
 
+		if assert.NoError(t, handler.JoinClass(&db.DBContext{
+			Context: c,
+			TLADB:   d,
+		})) {
+			require.Equal(t, http.StatusOK, rec.Code)
+		}
+		class, err := d.LoadClass(context.Background(), "test")
+		assert.Nil(t, err)
+		assert.NotZero(t, class)
+		assert.Equal(t, len(class.Members), 1)
+		assert.Equal(t, class.Members[0], "test")
+
+		user, err := d.LoadUser(context.Background(), "test")
+		assert.Nil(t, err)
+		assert.NotZero(t, user)
+		assert.Equal(t, len(user.Classes), 1)
+		assert.Equal(t, user.Classes[0], "test")
 	})
 }
