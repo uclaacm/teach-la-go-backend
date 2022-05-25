c25b1ec4019eeabd4ce24406cfc5abcea37c6173
Arsh <arshmalik02@gmail.com>
2022-05-24 18:54:45 -0700

    Comment out tests

--- db/program_test.go
+++ db/program_test.go
@@ -2,19 +2,15 @@ package db
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"strings"
 	"testing"
 
-	"cloud.google.com/go/firestore"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func TestUpdateProgram(t *testing.T) {
@@ -58,55 +54,6 @@ func TestUpdateProgram(t *testing.T) {
 		}
 	})
 	// TODO: more rigorous integration tests
-}
-
-func TestDeleteProgram(t *testing.T) {
-	d, err := Open(context.Background(), os.Getenv("TLACFG"))
-	require.NoError(t, err)
-
-	dbConsistencyWarning(t)
-	t.Log("this test will assume that the first userdoc pulled from staging has at least one program")
-
-	t.Run("TypicalRequest", func(t *testing.T) {
-		randomUser := User{}
-		err = d.RunTransaction(context.Background(), func(ctx context.Context, tx *firestore.Transaction) error {
-			// get some random user with more than one program
-			userDoc, err := d.Collection(usersPath).DocumentRefs(context.Background()).Next()
-			if err != nil {
-				return err
-			}
-			userSnap, err := tx.Get(userDoc)
-			if err != nil {
-				return err
-			}
-			return userSnap.DataTo(&randomUser)
-		})
-		require.NoError(t, err)
-
-		// request struct
-		request := struct {
-			UID string `json:"uid"`
-			PID string `json:"pid"`
-		}{
-			UID: randomUser.UID,
-			PID: randomUser.Programs[0],
-		}
-		t.Logf("trying to delete program with pid (%s)", request.PID)
-		programToDelete := d.Collection(programsPath).Doc(request.PID)
-		b, err := json.Marshal(&request)
-		require.NoError(t, err)
-
-		// try to delete it
-		req, rec := httptest.NewRequest(http.MethodDelete, "/", strings.NewReader(string(b))), httptest.NewRecorder()
-		c := echo.New().NewContext(req, rec)
-		if assert.NoError(t, d.DeleteProgram(c)) {
-			assert.Equal(t, http.StatusOK, rec.Code)
-
-			// check that the program actually was deleted
-			_, err := programToDelete.Get(context.Background())
-			assert.Equal(t, codes.NotFound, status.Code(err))
-		}
-	})
 }
 
 func TestForkProgram(t *testing.T) {
