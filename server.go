477e622aed116171233d3036b1bdb73b40198431
Joyce Chen <joyce.chen721@gmail.com>
2022-11-06 20:21:46 -0800

    initial commit

--- server.go
+++ server.go
@@ -88,7 +88,7 @@ func serve(c *cli.Context) error {
 	e.POST("/class/create", d.CreateClass)
 	e.PUT("/class/join", handler.JoinClass)
 	e.PUT("/class/leave", d.LeaveClass)
-	e.POST("/class/members", d.GetClassMembers)
+	e.POST("/class/members", handler.GetClassMembers)
 
 	// collaborative coding management
 	e.POST("/collab/create", d.CreateCollab)
